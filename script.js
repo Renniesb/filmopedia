9b101bb64ad4cda8a4368738bcd3307f04268c18
Rennie Bevineau <rsbevineau@gmail.com>
2020-04-28 20:43:26 -0400

    implement OMDB api and functionality to print results to the page

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-// http://www.omdbapi.com/?apikey=2a2afcb2&t=Godfather
+'use strict';
+
+
+const OMDBsearchURL = 'http://www.omdbapi.com/?apikey=2a2afcb2&t=';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayOMDBData(movie) {
+  let html = '';
+
+    html = html + `<h2>Movie Name: ${movie.Title}</h5><p>Plot: ${movie.Plot}</p>
+    <p>Year: ${movie.Year}</p>\n`;
+  
+
+  $('#results').html(html);
+
+
+}
+
+
+function getMovieData(query) {
+
+  const url = OMDBsearchURL + query;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+      console.log(JSON.stringify(responseJson))
+      displayOMDBData(responseJson);
+    })
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    getMovieData(searchTerm);
+  });
+}
+
+$(watchForm);
